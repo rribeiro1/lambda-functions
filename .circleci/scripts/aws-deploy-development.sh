7e062853abb5dbe2c50e6d4206c241b6b7b3ff44
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-04-11 15:28:18 +0200

    testing

--- .circleci/scripts/aws-deploy-development.sh
+++ .circleci/scripts/aws-deploy-development.sh
@@ -1,7 +1,5 @@
 unset  AWS_SESSION_TOKEN
-temp_role=$(aws sts assume-role \
-      --role-arn "arn:aws:iam::880088989213:role/circleci_deployment_role" \
-      --role-session-name "deployment" )
-export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
-export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
-export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
+temp_credentials=$(aws sts assume-role --role-arn "arn:aws:iam::$1:role/circleci_deployment_role" --duration-seconds 900 --role-session-name "deployment")
+export AWS_ACCESS_KEY_ID=$(echo "$temp_credentials" | jq .Credentials.AccessKeyId | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo "$temp_credentials" | jq .Credentials.SecretAccessKey | xargs)
+export AWS_SESSION_TOKEN=$(echo "$temp_credentials" | jq .Credentials.SessionToken | xargs)
